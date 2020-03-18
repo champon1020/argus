3c720192d59b2f4b6a5615756818308d72df54cf
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-03-18 11:40:25 +0900

    fix too

--- service/generator.go
+++ service/generator.go
@@ -29,15 +29,15 @@ func GenMask(st interface{}, fieldNames ...string) (mask uint32) {
 
 // Generate arguments slice used in database query.
 // If (flg & 1 << 31) > 0, limit query is turned on.
-func GenArgsSlice(argsFlg uint32, st interface{}, ol ...[2]int) (args []interface{}) {
+func GenArgsSlice(argsMask uint32, st interface{}, ol ...[2]int) (args []interface{}) {
 	v := reflect.Indirect(reflect.ValueOf(st))
 	t := v.Type()
 	for i := 0; i < t.NumField(); i++ {
-		if (1 << i & argsFlg) > 0 {
+		if (1 << i & argsMask) > 0 {
 			args = append(args, v.Field(i).Interface())
 		}
 	}
-	if (1<<31&argsFlg) > 0 && len(ol) >= 1 {
+	if (1<<31&argsMask) > 0 && len(ol) >= 1 {
 		args = append(args, ol[0][0], ol[0][1])
 	}
 	return
@@ -47,20 +47,20 @@ func GenArgsSlice(argsFlg uint32, st interface{}, ol ...[2]int) (args []interfac
 // If (flg & 1 << 31) > 0, limit query is turned on.
 // Return values is query(query of following 'WHERE')
 // and limit(limit query 'LIMIT ?,?').
-func GenArgsQuery(argsFlg uint32, st interface{}) (query string, limit string) {
+func GenArgsQuery(argsMask uint32, st interface{}) (query string, limit string) {
 	const initQuery = "WHERE "
 	query = initQuery
 	v := reflect.Indirect(reflect.ValueOf(st))
 	t := v.Type()
 	for i := 0; i < t.NumField(); i++ {
-		if (1 << i & argsFlg) > 0 {
+		if (1 << i & argsMask) > 0 {
 			if query != initQuery {
 				query += "AND "
 			}
 			query += ToSnakeCase(t.Field(i).Name) + "=" + "? "
 		}
 	}
-	if (1 << 31 & argsFlg) > 0 {
+	if (1 << 31 & argsMask) > 0 {
 		limit = "LIMIT ?,? "
 	}
 	if query == initQuery {
