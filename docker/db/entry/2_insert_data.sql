84c04949529ee52acc2fd5e39cf3bd25d516e4a9
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-04-08 11:45:57 +0900

    fix docker files and some files used on initialization of mysql

--- docker/db/entry/2_insert_data.sql
+++ docker/db/entry/2_insert_data.sql
@@ -21,3 +21,27 @@ LOAD DATA LOCAL INFILE
     CHARACTER SET utf8
     FIELDS TERMINATED BY ','
     ENCLOSED BY '"';
+
+USE argus;
+
+LOAD DATA LOCAL INFILE
+    "/docker/db/csv/articles.csv"
+    INTO TABLE articles
+    CHARACTER SET utf8
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    (id, title, create_date, update_date, content, image_hash, private);
+
+LOAD DATA LOCAL INFILE
+    "/docker/db/csv/categories.csv"
+    INTO TABLE categories
+    CHARACTER SET utf8
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"';
+
+LOAD DATA LOCAL INFILE
+    "/docker/db/csv/article_category.csv"
+    INTO TABLE article_category
+    CHARACTER SET utf8
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"';
